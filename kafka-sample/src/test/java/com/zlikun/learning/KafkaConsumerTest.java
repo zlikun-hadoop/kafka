bf68047ac245ac62ac8f73ec71b70cd1fccd06c1
zlikun <zlikun-dev@hotmail.com>
2017-11-14 16:08:38 +0800

    测试 KafkaConsumer (基本用法)

--- kafka-sample/src/test/java/com/zlikun/learning/KafkaConsumerTest.java
+++ kafka-sample/src/test/java/com/zlikun/learning/KafkaConsumerTest.java
@@ -19,14 +19,19 @@ public class KafkaConsumerTest {
 
     KafkaConsumer<String, String> consumer ;
     String servers = "192.168.120.74:9092" ;
+    String topic = "my-replicated-topic" ;
 
     @Before
     public void init() {
         Properties props = new Properties();
         props.put("bootstrap.servers", servers);
+        // consumer group id
         props.put("group.id", "test");
+        // 自动提交 offset
         props.put("enable.auto.commit", "true");
+        // 自动提交 offset 的时间间隔
         props.put("auto.commit.interval.ms", "1000");
+        // 与 producer 刚好相反，这里使用 Deserializer
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         consumer = new KafkaConsumer<>(props);
@@ -35,8 +40,10 @@ public class KafkaConsumerTest {
     @Test
     public void test() {
 
-        consumer.subscribe(Arrays.asList("my-replicated-topic"));
+        // 订阅 Topic
+        consumer.subscribe(Arrays.asList(topic, "test"));
         while (true) {
+            // 从 Topic 拉取消息，每次拉取可以是多条
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records)
                 System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
